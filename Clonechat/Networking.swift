b2facd49633d6d34aa13ef5a85116de00981cf1b
shreyashirday@gmail.com <shreyashirday@gmail.com>
2016-11-30 21:57:29 -0500

    added networking login and register calls

---
+++ Clonechat/Networking.swift
@@ -0,0 +1,57 @@
+//
+//  Networking.swift
+//  Hawk
+//
+//  Created by Shreyas Hirday on 11/12/16.
+//  Copyright © 2016 HirDaysOfTheWeek. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import AlamofireObjectMapper
+
+class Networking {
+    
+    var url: String? {
+        return "http://localhost:3000"
+    }
+
+    class func register(userId : String, email : String, password : String, completionHandler: @escaping (NSDictionary?, NSError?) -> ()) {
+        let registerUrl = Networking().url! + "/register"
+        let parameters : Parameters = [
+            "username" : userId,
+            "email" : email,
+            "password" : password
+        ]
+        Alamofire.request(registerUrl, method: .post, parameters: parameters).responseJSON { response in
+            
+            switch response.result {
+            case .success(let value):
+                completionHandler(value as? NSDictionary, nil)
+            case .failure(let error):
+                completionHandler(nil, error as NSError?)
+            }
+        }
+    }
+    
+    class func login(userId : String, password : String, completionHandler: @escaping (NSDictionary?, NSError?) -> () ) {
+        let loginUrl = Networking().url! + "/login"
+        let parameters : Parameters = [
+            "username" : userId,
+            "password" : password
+        ]
+        Alamofire.request(loginUrl, method: .post, parameters: parameters).responseJSON { response in
+            
+            switch response.result {
+            case .success(let value):
+                completionHandler(value as? NSDictionary, nil)
+            case .failure(let error):
+                completionHandler(nil, error as NSError?)
+            }
+        }
+    }
+    
+   
+    
+    
+}
---
+++ Podfile
@@ -0,0 +1,8 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, '10.0'
+use_frameworks!
+
+target 'Clonechat' do
+    pod 'Alamofire', '~> 4.0'
+    pod 'AlamofireObjectMapper', '~> 4.0'
+end
