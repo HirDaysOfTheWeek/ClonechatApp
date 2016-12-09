d58607296aa998607b7b5381d07856a91ae7bc75
Vineeth Puli <vpuli98@gmail.com>
2016-12-08 19:04:31 -0500

    register and login working

--- Clonechat/LoginViewController.swift
+++ Clonechat/LoginViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var registerBtn: UIButton!
+    @IBOutlet weak var loginBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +24,24 @@ class LoginViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func doLogin(_ sender: Any) {
+        let username = usernameField.text
+        let password = passwordField.text
+        
+        Networking.login(userId: username!, password: password!, completionHandler: {
+            response, error in
+            
+            let status = response?.status
+            if status == "ok" {
+                self.performSegue(withIdentifier: "loginSuccessful", sender: self)
+            } else {
+                let message = response?.message
+                print("message = \(message)")
+            }
+            
+        })
+        
+    }
     /*
     // MARK: - Navigation
 
